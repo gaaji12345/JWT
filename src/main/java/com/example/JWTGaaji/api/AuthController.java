2a7a03f0d8ff062af6ff6168694e6c5b95d8f473
gaji12345 <gajimax12345@gmail.com>
2024-09-18 16:56:02 +0530

    AuthController register and login method done

---
+++ src/main/java/com/example/JWTGaaji/api/AuthController.java
@@ -0,0 +1,74 @@
+package com.example.JWTGaaji.api;/*  gaajiCode
+    99
+    18/09/2024
+    */
+
+import com.example.JWTGaaji.dto.LoginDto;
+import com.example.JWTGaaji.dto.RegisterDto;
+import com.example.JWTGaaji.entity.Roles;
+import com.example.JWTGaaji.entity.UserE;
+import com.example.JWTGaaji.repo.RoleRepo;
+import com.example.JWTGaaji.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private AuthenticationManager authenticationManager;
+    private UserRepo userRepository;
+    private RoleRepo roleRepository;
+    private PasswordEncoder passwordEncoder;
+   // private JWTGenerator jwtGenerator;
+
+    @Autowired
+    public AuthController(AuthenticationManager authenticationManager, UserRepo userRepository,
+                          RoleRepo roleRepository, PasswordEncoder passwordEncoder) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+
+    }
+
+
+    @PostMapping("login")
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDto.getUsername(),
+                        loginDto.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<>("User Signed Success", HttpStatus.OK);
+    }
+    @PostMapping("register")
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+        if (userRepository.existsByUsername(registerDto.getUsername())) {
+            return new ResponseEntity<>("Username is taken!", HttpStatus.BAD_REQUEST);
+        }
+
+        UserE user = new UserE();
+        user.setUsername(registerDto.getUsername());
+        user.setPassword(passwordEncoder.encode((registerDto.getPassword())));
+
+        Roles roles = roleRepository.findByName("USER").get();
+        user.setRoles(Collections.singletonList(roles));
+
+        userRepository.save(user);
+
+        return new ResponseEntity<>("User registered success!", HttpStatus.OK);
+    }
+}
