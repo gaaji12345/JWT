6cf81eb4ccb7f5271b4a9cbc8f70302cab5c0775
gaji12345 <gajimax12345@gmail.com>
2024-09-18 14:48:11 +0530

    first commit

---
+++ src/main/java/com/example/JWTGaaji/JwtGaajiApplication.java
@@ -0,0 +1,13 @@
+package com.example.JWTGaaji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtGaajiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtGaajiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JWTGaaji/JwtGaajiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JWTGaaji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtGaajiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
