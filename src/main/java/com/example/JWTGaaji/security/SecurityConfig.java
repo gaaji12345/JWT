ac68c359d5c1ca174549cb0a72571ae001184c7b
gaji12345 <gajimax12345@gmail.com>
2024-09-18 21:32:25 +0530

    secrity pak done

--- src/main/java/com/example/JWTGaaji/security/SecurityConfig.java
+++ src/main/java/com/example/JWTGaaji/security/SecurityConfig.java
@@ -3,48 +3,88 @@ package com.example.JWTGaaji.security;/*  gaajiCode
     18/09/2024
     */
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private JwtAuthEntryPoint jwtAuthEntryPoint;
+    private CustomUserDetailsService userDetailsService;
+    @Autowired
+    public SecurityConfig(CustomUserDetailsService userDetailsService,JwtAuthEntryPoint jwtAuthEntryPoint) {
+        this.userDetailsService = userDetailsService;
+        this.jwtAuthEntryPoint=jwtAuthEntryPoint;
+
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
-                .requestMatchers(HttpMethod.GET).permitAll() // Allow registration and login
+                .requestMatchers("/api/auth/**").permitAll() // Allow registration and login
                 .anyRequest().authenticated() // Require authentication for any other request
                 .and()
                 . httpBasic();// Optional: if you want to use form-based login
+          http.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+
 
         return http.build();
     }
 
 
-    @Bean
-    public UserDetailsService users(){
-        UserDetails admin= User.builder()
-                .username("admin")
-                .password("password")
-                .roles("ADMIN")
-                .build();
+//    @Bean
+//    public UserDetailsService users(){
+//        UserDetails admin= User.builder()
+//                .username("admin")
+//                .password("password")
+//                .roles("ADMIN")
+//                .build();
+//
+//        UserDetails user= User.builder()
+//                .username("user")
+//                .password("password")
+//                .roles("USER")
+//                .build();
+//        return  new InMemoryUserDetailsManager(admin,user);
+//    }
 
-        UserDetails user= User.builder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-        return  new InMemoryUserDetailsManager(admin,user);
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public  JWTFilter jwtFilter(){
+        return new JWTFilter();
     }
 
 }
---
+++ src/main/java/com/example/JWTGaaji/security/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.example.JWTGaaji.security;/*  gaajiCode
+    99
+    18/09/2024
+    */
+
+public class SecurityConstants {
+    public static final long JWT_EXPIRATION = 70000;
+    public static final String JWT_SECRET="843567893696976453275974432697R634976R738467TR678T34865R6834R8763T478378637664538745673865783678548735687R3";
+}
